80bbc08b201124ead0b16cc0eeb1fe3649dadcb0
Richard Dalton <richard@codeinstitute.net>
2018-11-12 16:19:40 +0000

    Initial Commit

---
+++ blog/views.py
@@ -0,0 +1,78 @@
+from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.utils import timezone
+from .models import Post
+from .forms import PostForm
+from django.contrib.auth.decorators import permission_required
+
+def is_in_group(user, group_name):
+    return user.groups.filter(name=group_name).exists()
+
+
+def user_can_edit_post(request, post):
+    wrote_the_post = post.author == request.user
+    is_editor = is_in_group(request.user, 'editors')
+    superuser = request.user.is_superuser
+    return wrote_the_post or superuser or is_editor
+
+
+
+# Create your views here.
+def get_index(request):
+    posts = Post.objects.filter(published_date__lte = timezone.now())
+    return render(request, "blog/index.html", {'posts': posts})
+    
+    
+def read_post(request, id):
+    post = Post.objects.get(pk=id)
+    post.views += 1
+    post.save()
+
+    can_edit = user_can_edit_post(request, post)
+
+    return render(request, "blog/read_post.html", 
+        {
+            'post': post, 
+            'can_edit': can_edit
+        })
+
+
+@login_required
+def write_post(request):
+    if request.method=="POST":
+        form = PostForm(request.POST, request.FILES)
+        post = form.save(commit=False)
+        post.author = request.user
+        post.save()
+        return redirect(read_post, post.id)
+    else:
+        form = PostForm()
+        return render(request, "blog/post_form.html", {'form': form})
+
+
+
+def edit_post(request, id):
+    post = get_object_or_404(Post, pk=id)
+    if request.method=="POST":
+        form = PostForm(request.POST, request.FILES, instance=post)
+        form.save()
+        return redirect(read_post, id)
+    else:
+        form = PostForm(instance=post)
+        return render(request, "blog/post_form.html", {'form': form})
+
+
+
+@permission_required('blog.can_publish')
+def get_unpublished_posts(request):
+    posts = Post.objects.filter(published_date__gte = timezone.now())
+    return render(request, "blog/index.html", {'posts': posts})    
+
+
+
+@permission_required('blog.can_publish')
+def publish_post(request, id):
+    post = get_object_or_404(Post, pk=id)
+    post.published_date = timezone.now()
+    post.save()
+    return redirect(read_post, post.id)
---
+++ django_blog/urls.py
@@ -0,0 +1,36 @@
+"""django_blog URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.views.static import serve
+from django.conf import settings
+
+from blog.views import get_index, read_post, edit_post, write_post, publish_post, get_unpublished_posts
+from accounts.views import signup
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('signup/', signup, name='signup'),
+
+    path('', get_index, name='index'),
+    path('posts/unpublished', get_unpublished_posts, name='get_unpublished_posts'),
+    path('posts/write/', write_post, name='write_post'),
+    path('posts/<int:id>/', read_post, name='read_post'),
+    path('posts/<int:id>/edit/', edit_post, name='edit_post'),
+    path('posts/<int:id>/publish/', publish_post, name='edit_post'),
+    path('media/<path:path>', serve, {'document_root': settings.MEDIA_ROOT}),
+]
