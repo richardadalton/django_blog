bb797c1eb0be39c59aef6f840d24c1079ee907d1
Richard Dalton <richard@codeinstitute.net>
2018-11-14 12:13:58 +0000

    Add STATIC_ROOT

--- django_blog/settings/dev.py
+++ django_blog/settings/dev.py
@@ -4,6 +4,7 @@ from .base import *
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
+STATIC_ROOT = 'staticfiles'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
